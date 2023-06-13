5318e7c8bbd06d2f5e7bf45c8531cfff33fbe20c
MohammadHossein R Shojaodiny <83103488+Reynardd@users.noreply.github.com>
2023-06-13 14:39:26 +0330

    high dpi scaling test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QFontDatabase>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     this->setStyleSheet("#centralwidget{background-image: url(:/back/background.jpg);}");
+
 }
 
 MainWindow::~MainWindow()
