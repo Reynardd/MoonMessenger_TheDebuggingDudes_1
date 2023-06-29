5bdd713d641ad88d67b288b91c28e6a95ef19933
MohammadHossein R Shojaodiny <83103488+Reynardd@users.noreply.github.com>
2023-06-29 19:39:41 +0330

    Request timeout
    added 5 sec timeout to request get'

--- request.cpp
+++ request.cpp
@@ -1,32 +1,43 @@
 #include "request.h"
+#include <QTimer>
+
 QJsonObject get(QString url, QUrlQuery params)
 {
-    QNetworkAccessManager* manager = new QNetworkAccessManager();
+    QNetworkAccessManager manager;
     QUrl requestURL(url);
     requestURL.setQuery(params);
-    QNetworkRequest request;
-    request.setUrl(requestURL);
-    QNetworkReply* reply = manager->get(request);
-    qDebug() << "url: "<<requestURL.toDisplayString();
+    QNetworkRequest request(requestURL);
+    QNetworkReply* reply = manager.get(request);
+    qDebug() << "url: " << requestURL.toDisplayString();
     QEventLoop eventLoop;
+    QTimer timeoutTimer;
+    timeoutTimer.setSingleShot(true);
     QObject::connect(reply, &QNetworkReply::finished, &eventLoop, &QEventLoop::quit);
+    QObject::connect(&timeoutTimer, &QTimer::timeout, &eventLoop, &QEventLoop::quit);
+    timeoutTimer.start(5000);//timeout duratuin 5 seconds
     eventLoop.exec();
-
     QJsonObject jsonObject;
-
-    if (reply->error())
-    {
-        qDebug() << reply->errorString();
-    }
-    else
-    {
-        QByteArray response = reply->readAll();
-        QJsonDocument doc(QJsonDocument::fromJson(response));
-        jsonObject = doc.object();
-        qDebug() << QString(doc.toJson(QJsonDocument::Compact)) <<"\n";
+    if (timeoutTimer.isActive()) {
+        timeoutTimer.stop();
+        if (reply->error() != QNetworkReply::NoError) {
+            qDebug() << "Error: " << reply->errorString();
+        } else {
+            QByteArray response = reply->readAll();
+            QJsonDocument doc = QJsonDocument::fromJson(response);
+            if (!doc.isNull()) {
+                if (doc.isObject()) {
+                    jsonObject = doc.object();
+                    qDebug() << QString(doc.toJson(QJsonDocument::Compact)) << "\n";
+                } else {
+                    qDebug() << "Error: Invalid JSON object received";
+                }
+            } else {
+                qDebug() << "Error: Failed to parse JSON response";
+            }
+        }
+    } else {
+        qDebug() << "Error: Request timed out";
     }
     reply->deleteLater();
-    delete manager;
-
     return jsonObject;
 }
